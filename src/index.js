1396eeb3dffb68c610dd1a245ca9ad46a8d798a3
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-08-29 05:09:27 +0000

    CreateMarkup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-import { setupServer } from "../src/server.js";
-import { initMongoConnection } from "./db/initMongoConnection.js";
-
-const bootstrap = async () => {
-    await initMongoConnection();
-    setupServer();
-  };
-  
-  bootstrap();
 
 
