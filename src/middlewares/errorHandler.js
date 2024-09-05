c2b180bf706027ce33d95bbc9fc1534d524415bb
Ihor Matviienko <igor.matvienko1989@gmail.com>
2024-09-05 15:02:53 +0300

    update

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const errorHandler = (error, req, res, next) => {
   const { status = 500, message } = error;
   res.status(status).json({
     message: 'Something went wrong',
-    data,
+    data: error.message,
   });
 };
 
