4c81b4b72551c6a3117e77929ddf3cb8aca0f1fb
Ihor Matviienko <igor.matvienko1989@gmail.com>
2024-09-15 09:45:36 +0300

    update

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,18 +1,20 @@
-import Joi from "joi";
-import { contactType, phoneNumberRegExp } from "../constants/contacts.js";
+import Joi from 'joi';
+import { contactType, phoneNumberRegExp } from '../constants/contacts.js';
 
 export const contactAddSchema = Joi.object({
-   name: Joi.string().min(3).max(20).required(),
-   phoneNumber: Joi.string().length(13).pattern(phoneNumberRegExp).required(),
-   email: Joi.string().email(),
-   isFavourite: Joi.boolean().required(),
-   contactType: Joi.string().valid(...contactType).required() 
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().length(13).pattern(phoneNumberRegExp).required(),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string()
+    .valid(...contactType)
+    .required(),
 });
 
 export const contactPatchSchema = Joi.object({
-    name: Joi.string().min(3).max(20),
-    phoneNumber: Joi.string().length(13).pattern(phoneNumberRegExp),
-    email: Joi.string().email(),
-    isFavourite: Joi.boolean(),
-    contactType: Joi.string().valid(...contactType)
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().length(13).pattern(phoneNumberRegExp),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid(...contactType),
 });
