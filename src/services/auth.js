a87867140646af70ce38b0c237be6b2482075a16
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-09-26 05:21:04 +0000

    update

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,21 +1,16 @@
 import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import { randomBytes } from 'crypto';
-
 import jwt from 'jsonwebtoken';
-
-import { SMTP, TEMPLATES_DIR } from '../constants/index.js';
+import { TEMPLATES_DIR } from '../constants/index.js';
 import { env } from '../utils/env.js';
 import { sendEmail } from '../utils/sendMail.js';
-
 import SessionCollection from '../db/models/Session.js';
 import UserCollection from '../db/models/User.js';
-
 import {
   accessTokenLifetime,
   refreshTokenLifetime,
 } from '../constants/users.js';
-
 import handlebars from 'handlebars';
 import path from 'node:path';
 import fs from 'node:fs/promises';
@@ -128,7 +123,7 @@ export const sendResetEmail = async (email) => {
   });
 
   await sendEmail({
-    from: env(SMTP.SMTP_FROM),
+    from: env("SMTP_FROM"),
     to: email,
     subject: 'Reset your password',
     html,
@@ -141,7 +136,7 @@ export const resetPassword = async (payload) => {
   try {
     entries = jwt.verify(payload.token, env('JWT_SECRET'));
   } catch (err) {
-    if (err instanceof Error) throw createHttpError(401, err.message);
+    if (err instanceof Error) throw createHttpError(401, "Token is expired or invalid.");
     throw err;
   }
 
